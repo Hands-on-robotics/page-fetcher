bd9cc1c583f15064e9fdd2e3827e19224bf1882f
Hands-on-robotics <handsonevanoff@gmail.com>
2023-09-20 14:40:44 -0700

    Created .gitignore and Deleted fetcher2.js

--- fetcher.js
+++ fetcher.js
@@ -4,8 +4,9 @@ const input = process.argv.slice(2, 4);
 const siteUrl = input[0];
 const filePathForSaving = input[1];
 
-// This function will call savingFileToLocal
+// uses request() and then a callback function to handle body
 const fetchingFileFromURL = function(siteUrl, callback) {
+  // only body is needed.
   request(siteUrl, (err, response, body) => {
     if (err) {
       console.log(err);
@@ -15,36 +16,23 @@ const fetchingFileFromURL = function(siteUrl, callback) {
   });
 };
 
-// To call in fetching file
+// uses fs.write to write the file on local
 const saveFileToLocal = (filePathForSaving, body) => {
   fs.writeFile(filePathForSaving, body, err => {
     if (err) {
       console.error(err);
     } else {
-      console.log(fs.body.Stats)
-      console.log(`Downloaded and saved ${body.length} bytes to ${filePathForSaving}`);
+      // uses fs.stat to show file size
+      fs.stat(filePathForSaving, (err, stats) => {
+        if (err) {
+          console.log(err);
+        } else {
+          const fileSize = stats.size;
+          console.log(`Downloaded and saved ${fileSize} bytes to ${filePathForSaving}`);
+        }
+      });
     }
   });
 };
 
 fetchingFileFromURL(siteUrl,saveFileToLocal);
-
-
-
-//request();
-
-// layout
-// request file body
-// fs.write(fileBody);
-// console.log(Downloaded and saved 32891 bytes to index.js)
-
-// Takes in two arguments
-// 1. url
-// 2. local file path
-
-// console logs message "Downloaded and saved 3294 bytes to ./index.html"
-
-// Two asynchronous operations
-// make http request and wait for response.
-// upon connection take data and write it into your local file system.
-
